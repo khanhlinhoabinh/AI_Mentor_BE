909458a3d62f4f6e33c619a893ddb7c376ea398f
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2026-05-07 21:18:55 +0700

    first commit

---
+++ src/main/java/com/aimentor/ai_mentor_be/AiMentorBeApplication.java
@@ -0,0 +1,13 @@
+package com.aimentor.ai_mentor_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiMentorBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiMentorBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aimentor/ai_mentor_be/AiMentorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimentor.ai_mentor_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiMentorBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
